c446653d2df94179094f7be697541e695619f7db
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-07-10 22:33:46 -0300

    Primer commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  @Get()
+  getTasks(): { hello: string } {
+    return { hello: 'world' };
+  }
+
+  @Post()
+  createTask(@Body() task: CreateTaskDto): string {
+    return 'Creating a task...';
+  }
+
+  @Put(':id')
+  updateTask(@Body() task: CreateTaskDto, @Param('id') id): string {
+    console.log(task);
+    console.log(id);
+    return 'Updating a task...';
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id): string {
+    console.log(id);
+    return `Deleting task #${id}`;
+  }
+}
